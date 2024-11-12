15cb4a46319d4a094279ad124328aa9b0d252a3a
Aman verma <amanverma.va@gmail.com>
2024-11-12 15:23:11 +0530

    adding plus and3dots to title

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Card from './Card';
+import threedots from '../assests/3 dot menu.svg'
+import addw from '../assests/add.svg'
 // import StatusImage from './StatusImage'
 
 function Column({ title, tickets }) {
   return (
     <div className="column">
-      <h2>{title}</h2>
+      <h2>{title} 
+        <div className="plusdot">
+        <img src={addw} alt="" />
+        <img src={threedots} alt="" />
+        </div>
+        </h2>
       
       {tickets.map((ticket) => (
         <Card key={ticket.id} ticket={ticket} />
